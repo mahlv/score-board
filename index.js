47fd30e4b09b14fb3e443b84ebac1c10842e93d1
mahlv <matheusholv@gmail.com>
2025-01-02 08:15:12 -0300

    Score Board
    Score Tracker

---
+++ index.js
@@ -0,0 +1,37 @@
+let addPoint = document.getElementById("homecount-el")
+let addPointGuest = document.getElementById("guestcount-el")
+let add = 0
+let addGuest = 0
+
+
+function add1(){
+    add += 1
+    addPoint.textContent = add
+}
+
+function add2(){
+    add += 2
+    addPoint.textContent = add
+}
+
+function add3(){
+    add += 3
+    addPoint.textContent = add
+}
+
+
+
+function addguest1(){
+    addGuest += 1
+    addPointGuest.textContent = addGuest
+}
+
+function addguest2(){
+    addGuest += 2
+    addPointGuest.textContent = addGuest
+}
+
+function addguest3(){
+    addGuest += 3
+    addPointGuest.textContent = addGuest
+}
